d97768117d39e3b952a2fac66092998a22667a8c
radhesh <radhesh@radhesh-OptiPlex-7010.(none)>
2014-04-04 14:56:10 +0530

    Code to start save stop and resume using python libvirt

---
+++ resume_domu.py
@@ -0,0 +1,19 @@
+import libvirt
+import sys
+
+def usage():
+  print "Usage : python start.py <domian_name>"
+  sys.exit(-1)
+
+if __name__ == "__main__":
+
+  try:
+    domain_name =  sys.argv[1]
+  except:
+    usage()
+
+  #connect to hypervisor running on localhost
+  conn = libvirt.open('xen:///')
+  # domain name : 'ubuntu-12.04'
+  dom0 = conn.lookupByName(domain_name)
+  dom0.resume()
